324d162bc2c028af5f5aae880dc7da9151c9732f
codeation <39458623+codeation@users.noreply.github.com>
2024-06-19 04:56:25 +0200

    Add motion event pointer

--- examples/geo/geo.go
+++ examples/geo/geo.go
@@ -14,12 +14,14 @@ var (
 	background = color.RGBA{255, 255, 255, 255}
 	red        = color.RGBA{255, 0, 0, 127}
 	blue       = color.RGBA{0, 0, 255, 255}
+	gray       = color.RGBA{0, 0, 0, 64}
 )
 
 func main() {
 	app := impress.NewApplication(image.Rect(0, 0, 640, 480), "Try to resize window")
 	defer app.Close()
 
+	point := image.Point{}
 	windowRect := image.Rect(0, 0, 640, 480)
 	w := app.NewWindow(windowRect, background)
 	defer w.Drop()
@@ -44,6 +46,9 @@ func main() {
 			w.Line(image.Pt(199+2, 100), image.Pt(199+2, 200), red) // right
 			w.Line(image.Pt(100, 199+2), image.Pt(200, 199+2), red) // down
 
+			w.Line(image.Pt(0, point.Y), image.Pt(windowRect.Max.X, point.Y), gray)
+			w.Line(image.Pt(point.X, 0), image.Pt(point.X, windowRect.Max.Y), gray)
+
 			w.Show()
 			app.Sync()
 		}
@@ -51,6 +56,9 @@ func main() {
 		e := <-app.Chan()
 		if e == event.DestroyEvent || e == event.KeyExit {
 			break
+		}
+		if motionEvent, ok := e.(event.Motion); ok {
+			point = motionEvent.Point
 		}
 		if resizeEvent, ok := e.(event.Configure); ok {
 			windowRect.Max = resizeEvent.InnerSize
